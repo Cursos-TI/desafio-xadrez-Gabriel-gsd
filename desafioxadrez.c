40c65a1ead861322b9f0148b185597138ed54f4f
Gabriel-gsd <ggaygher@hotmail.com>
2025-10-29 00:28:45 +0000

    Nível Mestre!

--- desafioxadrez.c
+++ desafioxadrez.c
@@ -1,39 +1,62 @@
 #include <stdio.h>
 
+// Recursividades
+
+//Torre
+void moverTorre(int casas1) {
+    if (casas1 > 0) {
+        printf("Direita \n");
+        moverTorre(casas1 - 1);
+    }
+}
+
+//Rainha
+void moverRainha (int casas2){
+    if (casas2 > 0) {
+        printf ("Esquerda \n");
+        moverRainha (casas2 - 1);
+    }
+}
+
+//Bispo
+void moverBispo (int casas3){
+        for (int a = 0; a < 5; a++){
+            for (int b = 0; b < 1; b++){
+                printf ("Direita ");
+            }
+            printf ("Cima \n");
+        }
+    }
+
 int main (){
 
     // Declaração das variáveis:
-    int torre, bispo = 0, rainha = 0;
+    int bispo = 0, rainha = 0, cavalo = 0;
 
-    // Estrutura for para a torre:
-    printf ("Movimentação da Torre: \n");
-    for (torre = 0; torre < 5; torre++){
-        printf ("Direita \n");
-    }
+    // Chamando a Recursividade "Mover Torre":
+    printf ("Movimentação da Torre: \n\n");
+    moverTorre(5);
 
-    // Estrutura while para o bispo:
-    printf ("\nMovimentação do Bispo: \n");
-    while (bispo < 5){
-        printf ("Direita ");
-        printf ("Cima \n");
-        bispo++;  
-    }
+     // Chamando a Recursividade "Mover Rainha":
+    printf ("\nMovimentação da Rainha: \n\n");
+    moverRainha (8);
 
-    // Estrutura do-while para a rainha:
-    printf ("\nMovimentação da Rainha: \n");
-    do {
-        printf ("Esquerda \n");
-        rainha ++;
-    } while (rainha < 8);
+    // Chamando a Recursividade "Mover Bispo":
+    printf ("\nMovimentação do Bispo: \n\n");
+    moverBispo (5);
 
     // Loop aninhado para o cavalo:
-    printf ("\nMovimentação do Cavalo: \n");
-    while (cavalo < 1){
-        for (int i = 0; i < 2; i++){
-            printf ("Baixo\n");
+    printf ("\nMovimentação do Cavalo: \n\n");
+    for (int c = 0; c <= 1; c++){
+        for (int d = 0; d <= 0; d++){
+            if (c <= 1){
+                printf ("Cima \n");
+            }
+            if (c == 1 & d == 0){
+                printf ("Direita\n");
+                break;
+            }
         }
-        cavalo++;
-        printf ("Esquerda\n");
     }
 
   return 0;
